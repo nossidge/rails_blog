d3b90d0ddfff77563bfc7abab5ef185b742000ee
Paul Thompson <nossidge@gmail.com>
2017-03-30 17:26:28 +0100

    basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'posts#index'
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
