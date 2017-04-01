605816560a010fbde4310061a98fc71285d93d86
Paul Thompson <nossidge@gmail.com>
2017-04-01 15:01:00 +0100

    display user name of poster, instead of id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit]
   before_action :set_markdown, only: [:index, :show]
 
   # GET /posts
@@ -70,6 +71,10 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def set_user
+      @user = User.find(@post.user_id)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
