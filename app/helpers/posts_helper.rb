1e1e33447368848bd41ba9fe2613aea2346736ea
Paul Thompson <nossidge@gmail.com>
2017-04-01 15:10:24 +0100

    a bit more DRY

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+
+  # Display any edits as hover-over title text.
+  # If the post has been edited, add an asterisk to the date.
+  def get_date_display_text(post)
+    output = {}
+
+    if post.created_at === post.updated_at
+      output[:value] = post.created_at.strftime("%Y/%m/%d at %H:%M")
+      output[:title] = "No edits made"
+    else
+      output[:value] = post.created_at.strftime("%Y/%m/%d at %H:%M") + "*"
+      output[:title] = "Last edited: #{post.updated_at.strftime("%Y/%m/%d %H:%M")}"
+    end
+
+    output
+  end
+
 end
