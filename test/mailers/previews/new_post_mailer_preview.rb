f0cc82f82754f7c82fe9a2ecd5fecf2f4b6ae790
Paul Thompson <nossidge@gmail.com>
2017-04-01 17:41:04 +0100

    add mailer preview

--- test/mailers/previews/new_post_mailer_preview.rb
+++ test/mailers/previews/new_post_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/new_post_mailer
 class NewPostMailerPreview < ActionMailer::Preview
-
+  def send_to_subscribers
+    NewPostMailer.send_to_subscribers(Post.first.id)
+  end
 end
