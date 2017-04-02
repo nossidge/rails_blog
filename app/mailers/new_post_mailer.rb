52f11925a15d3ed8e519e829bb3b20475899134a
Paul Thompson <nossidge@gmail.com>
2017-04-02 10:21:05 +0100

    remove the old mail action

--- app/mailers/new_post_mailer.rb
+++ app/mailers/new_post_mailer.rb
@@ -1,22 +1,6 @@
 class NewPostMailer < ApplicationMailer
   default from: ENV['gmail_sendname']
 
-
-  # Sends email to all subscribers when new post is created.
-  # Send the mail to the sending email address, and bcc all the subscribers.
-  # Upside of this approach; it will just make one email action.
-  # Downside; we won't be able to personalise the email.
-  def send_to_subscribers(id)
-    @post = Post.find(id)
-    mail( to: ENV['gmail_username'],
-          bcc: Subscriber.pluck(:email),
-          subject: "Paul's Lovely Rails Blog: #{@post.title}")
-  end
-
-
-  # ToDo: Need to personalise these eventually, so we can add an 'unsubscribe' link.
-
-
   # Sends email to a specific subscriber.
   def send_to_one_subscriber(post_id, subscriber_id)
     @post = Post.find(post_id)
@@ -24,6 +8,5 @@ class NewPostMailer < ApplicationMailer
     mail( to: @subscriber.email,
           subject: "Paul's Lovely Rails Blog: #{@post.title}")
   end
-
 
 end
--- test/mailers/previews/new_post_mailer_preview.rb
+++ test/mailers/previews/new_post_mailer_preview.rb
@@ -1,8 +1,5 @@
 # Preview all emails at http://localhost:3000/rails/mailers/new_post_mailer
 class NewPostMailerPreview < ActionMailer::Preview
-  def send_to_subscribers
-    NewPostMailer.send_to_subscribers(Post.first.id)
-  end
   def send_to_one_subscriber
     NewPostMailer.send_to_one_subscriber(Post.first.id, Subscriber.first.id)
   end
