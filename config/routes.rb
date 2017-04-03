05cdb6ecd7944de503f755894a9dcbf9a85f9fd7
Paul Thompson <nossidge@gmail.com>
2017-04-03 12:37:05 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   get 'pages/home'
+  get 'home', to: 'pages#home'
 
   get 'pages/about'
+  get 'about', to: 'pages#about'
 
+  # Todo: This static page is unnecessary.
   get 'pages/subscribe'
+  get 'subscribe', to: 'pages#subscribe'
 
   resources :subscribers do
     get 'unsubscribe', on: :member
@@ -13,6 +17,6 @@ Rails.application.routes.draw do
   end
   resources :users
 
-  root 'posts#index'
+  root 'pages#home'
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
